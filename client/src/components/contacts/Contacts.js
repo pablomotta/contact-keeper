54527c46d7278fc21d9b5255ba5884f657d87617
Pablo Motta <pablomotta@PMBP.local>
2019-08-15 15:30:22 -0700

    Finished fronend

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,42 @@
+import React, { Fragment, useContext } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import ContactItem from './ContactItem';
+import ContactContext from '../../context/contact/contactContext';
+
+const Contacts = () => {
+    const contactContext = useContext(ContactContext);
+
+    const { contacts, filtered } = contactContext;
+
+    if (contacts.length === 0) {
+        return <h3>Please add a contact</h3>;
+    }
+
+    return (
+        <Fragment>
+            <TransitionGroup>
+                {filtered !== null
+                    ? filtered.map(contact => (
+                          <CSSTransition
+                              key={contact.id}
+                              timeout={500}
+                              classNames='item'
+                          >
+                              <ContactItem contact={contact} />
+                          </CSSTransition>
+                      ))
+                    : contacts.map(contact => (
+                          <CSSTransition
+                              key={contact.id}
+                              timeout={500}
+                              classNames='item'
+                          >
+                              <ContactItem contact={contact} />
+                          </CSSTransition>
+                      ))}
+            </TransitionGroup>
+        </Fragment>
+    );
+};
+
+export default Contacts;
